21d2b6669f464fca495943f0ea3ff216b4e4af79
Fatmaual <fatmaulusal@gmail.com>
2020-04-10 16:06:48 -0400

    login page updated

--- src/test/java/com/automation/bitrix/pages/LoginPage.java
+++ src/test/java/com/automation/bitrix/pages/LoginPage.java
@@ -8,79 +8,50 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-        @FindBy(name = "USER_LOGIN")
-        private WebElement userName;
+    @FindBy(name = "USER_LOGIN")
+    private WebElement userName;
 
-        @FindBy(name="USER_PASSWORD")
-        private WebElement password;
+    @FindBy(name = "USER_PASSWORD")
+    private WebElement password;
 
-        @FindBy(id = "USER_REMEMBER")
-        private WebElement rememberMe;
+    @FindBy(id = "USER_REMEMBER")
+    private WebElement rememberMe;
 
-        @FindBy(className = "login-btn")
-        private WebElement login;
+    @FindBy(className = "login-btn")
+    private WebElement login;
 
-        @FindBy(className = "login-link-forgot-pass")
-        private WebElement forgotPassword;
+    @FindBy(className = "login-link-forgot-pass")
+    private WebElement forgotPassword;
 
-        @FindBy(name="USER_EMAIL")
-        private WebElement loginWithEmail;
+    @FindBy(name = "USER_EMAIL")
+    private WebElement loginWithEmail;
 
-        @FindBy(className = "login-links")
-        private WebElement authorization;
+    @FindBy(className = "login-links")
+    private WebElement authorization;
 
-        @FindBy(className = "login-btn")
-        private WebElement resetPassword;
+    @FindBy(className = "login-btn")
+    private WebElement resetPassword;
 
-        public LoginPage(){
-            PageFactory.initElements(Driver.getDriver(),this);
-        }
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
-
-        public void loginHelp_desk1(){
-            userName.sendKeys(ConfigurationReader.getProperty("help_desk1"));
-            password.sendKeys(ConfigurationReader.getProperty("password"));
-
-        }
-
-        public void loginHelp_desk2(){
-            userName.sendKeys(ConfigurationReader.getProperty("help_desk2"));
-            password.sendKeys(ConfigurationReader.getProperty("password"));
-
-        }
-
-        public void loginMarketing1(){
-            userName.sendKeys(ConfigurationReader.getProperty("marketing1"));
-            password.sendKeys(ConfigurationReader.getProperty("password"));
-
-        }
-
-        public void loginMarketing2(){
-            userName.sendKeys(ConfigurationReader.getProperty("marketing2"));
-            password.sendKeys(ConfigurationReader.getProperty("password"));
-
-        }
-
-        public void loginHr1(){
-            userName.sendKeys(ConfigurationReader.getProperty("hr1"));
-            password.sendKeys(ConfigurationReader.getProperty("password"));
-
-        }
-
-        public void loginHr2(){
-            userName.sendKeys(ConfigurationReader.getProperty("hr2"));
-            password.sendKeys(ConfigurationReader.getProperty("password"));
-
-        }
-
-        public void loginAs(String userValue, String passwordValue){
-            userName.sendKeys(userValue);
-            password.sendKeys(passwordValue);
-            login.click();
-
-        }
+    public void loginAs(String userValue, String passwordValue) {
+        userName.sendKeys(userValue);
+        password.sendKeys(passwordValue);
+        login.click();
 
     }
+
+    public void loginAs(String userType) {
+
+        userName.sendKeys(ConfigurationReader.getProperty(userType));
+        password.sendKeys(ConfigurationReader.getProperty("password"));
+        login.click();
+
+    }
+
+}
 
 
 
