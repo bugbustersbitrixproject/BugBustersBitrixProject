66522a2fb814fbdd81a0a3b71e5587352803b3b8
Huseyin Civan <huseyincivan@gmail.com>
2020-04-13 13:53:38 -0400

    update

--- src/test/java/com/automation/bitrix/pages/LoginPage.java
+++ src/test/java/com/automation/bitrix/pages/LoginPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage extends AbstractPageBase {
 
     @FindBy(name = "USER_LOGIN")
     private WebElement userName;
