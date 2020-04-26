031cfab8128511734f208fec9cba4cf3c29930bc
Agolcu <golcuayse@hotmail.com>
2020-04-26 18:20:40 -0400

    adding method

--- src/test/java/com/automation/bitrix/tests/activityStream/EventTabTest.java
+++ src/test/java/com/automation/bitrix/tests/activityStream/EventTabTest.java
@@ -6,6 +6,7 @@ import com.automation.bitrix.pages.activityStream.FileTab;
 import com.automation.bitrix.tests.AbstractTestBase;
 import com.automation.utilities.BrowserUtils;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
@@ -75,7 +76,7 @@ public class EventTabTest extends AbstractTestBase {
         eventTab.uploadFile("Encapsulation.txt", "11111111.jpg");
         test.info("Encapsulation.txt and  11111111.jpg files uploaded");
 
-        eventTab.sendFiles();
+        eventTab.sendSubmit();
         Assert.assertTrue(eventTab.uplodedFileName("Encapsulation.txt").contains(eventTab.uplodedFileName("Encapsulation.txt")));
         eventTab.clickOnImage();
         BrowserUtils.wait(3);
@@ -100,7 +101,7 @@ public class EventTabTest extends AbstractTestBase {
 
         eventTab.attachUrlLink("https://qa2.vytrack.com/user/login");
         BrowserUtils.wait(3);
-        eventTab.sendFiles();
+        eventTab.sendSubmit();;
         BrowserUtils.wait(3);
 
         String urlactual = eventTab.attachedLinkName("https://qa2.vytrack.com/user/login");
@@ -108,8 +109,68 @@ public class EventTabTest extends AbstractTestBase {
         String urlExpected = "https://qa2.vytrack.com/user/login";
         BrowserUtils.wait(3);
         Assert.assertEquals(urlactual, urlExpected);
-
         test.pass("url attached successfully");
     }
+
+@Test(description = "User should be able to create a quote by clicking on the Comma icon")
+    public void createQuote(){
+    test = report.createTest("Attach Url Link");
+
+    LoginPage loginPage = new LoginPage();
+    loginPage.loginAs("help_desk2");
+    test.info("Login successfully");
+    EventTab eventTab = new EventTab();
+    eventTab.navigateToTab("Event");
+    test.info("Navigated to EventTab");
+    BrowserUtils.wait(3);
+    eventTab.clickQuoteIcon();
+    eventTab.sendQuoteInput("Hello BugBusters");
+    eventTab.sendSubmit();
+    Assert.assertEquals(eventTab.getSendQuote(),"Hello BugBusters");
+
+    test.pass("Quote created successfully");
+    }
+
+    @Test(description = "User should able to click Visual editor icon and see text_bar")
+    public void visualEditor(){
+        test = report.createTest("Attach Url Link");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAs("help_desk2");
+        test.info("Login successfully");
+        EventTab eventTab = new EventTab();
+        eventTab.navigateToTab("Event");
+        test.info("Navigated to EventTab");
+        BrowserUtils.wait(3);
+        eventTab.clickVisualEditor();
+        BrowserUtils.wait(1);
+        for(int i=0;i<eventTab.getText_bar().size();i++) {
+            Assert.assertTrue(eventTab.getText_bar().get(i).isDisplayed());
+
+        }
 }
-//https://www.youtube.com/watch?v=niQ0A1xz0Os
+        @Test(description = "User should be able to add Event start and ending date and time, and specify the time zone.")
+        public void dateEditor(){
+            test = report.createTest("Attach Url Link");
+
+            LoginPage loginPage = new LoginPage();
+            loginPage.loginAs("help_desk2");
+            test.info("Login successfully");
+            EventTab eventTab = new EventTab();
+            eventTab.navigateToTab("Event");
+            test.info("Navigated to EventTab");
+            BrowserUtils.wait(3);
+            eventTab.setFromDate("05/23/2020");
+            eventTab.setDateTo("05/23/2020");
+            eventTab.setFromTime("10:30 am");
+            eventTab.setTimeTo("11:30 pm");
+            eventTab.chooseEventLocation("Central Meeting Room");
+            eventTab.sendSubmit();
+            String expected="05/23/2020, 10:30 am";
+            String actual=eventTab.eventDateAndTime();
+            Assert.assertEquals(actual,expected);
+            String expectedRoom="Central Meeting Room";
+            String actualRoom=eventTab.meetingLocation();
+            Assert.assertEquals(actualRoom,expectedRoom);
+//
+}}
